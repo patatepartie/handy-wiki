479e964e604471d08f38205db5a9489a47f5f7b5
Cyril Ledru <cyril.ledru@gmail.com>
2010-11-22 22:19:50 +0100

    Better readability of installer

--- src/main/java/fr/patatepartie/handy/Installer.java
+++ src/main/java/fr/patatepartie/handy/Installer.java
@@ -15,24 +15,45 @@ import com.google.common.io.Files;
 
 public class Installer {
 
+	private static final String REPOSITORY_SUB_DIRECTORY = "repo";
+	
+	private File installationDirectory;
+	private String originPath;
+	private Repository repository;
+
+	public Installer(File installationDirectory, String originPath) {
+		this.installationDirectory = installationDirectory;
+		this.originPath = originPath;
+	}
+
 	public static void main(String[] args) throws IOException, URISyntaxException {
-		String installationDirectoryPath = args[0];
-		String centralGitRepositoryPath = args[1];
-		File installationDirectory = new File(installationDirectoryPath);
+		File installationDirectory = new File(args[0]);
+		String originPath = args[1];
 		
+		Installer installer = new Installer(installationDirectory, originPath);
+		installer.createInstallationDirectory();
+		installer.createGitRepository();
+		installer.addOriginRepository();
+	}
+
+	private void createInstallationDirectory() throws IOException {
 		Files.createParentDirs(new File(installationDirectory, "anyChild"));
-		Repository repository = new FileRepositoryBuilder()
-								.setWorkTree(new File(installationDirectory, "repo"))
+	}
+
+	private void createGitRepository() throws IOException {
+		repository = new FileRepositoryBuilder()
+								.setWorkTree(new File(installationDirectory, REPOSITORY_SUB_DIRECTORY))
 								.build();
-		
 		repository.create();
+	}
+	
+	private void addOriginRepository() throws URISyntaxException, IOException {
 		StoredConfig repositoryConfig = repository.getConfig();
-		
 		repositoryConfig.setString("branch", "master", "remote", "origin");
 		repositoryConfig.setString("branch", "master", "merge", "refs/heads/master");
-
+		
 		RemoteConfig remoteConfig = new RemoteConfig(repositoryConfig, "origin");
-		remoteConfig.addURI(new URIish(centralGitRepositoryPath));
+		remoteConfig.addURI(new URIish(originPath));
 		remoteConfig.addFetchRefSpec(new RefSpec("+refs/heads/*:refs/remotes/origin/*"));
 		
 		remoteConfig.update(repositoryConfig);
