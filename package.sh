b46e103fc25af49e8fd0bf606180f974c98a8fe0
Cyril <cyril.ledru@gmail.com>
2010-12-12 22:40:42 +0100

    Clean before building project

--- package.sh
+++ package.sh
@@ -5,10 +5,16 @@ setupVariables() {
 	INSTALLER="src/main/assembly/installer.sh"
 	SCRIPTS_DIR="src/main/scripts"
 	PAGE_CREATION_SCRIPT="${SCRIPTS_DIR}/handy-wiki.sh"
-	FULL_INSTALLER="target/handy-installer.sh"
+	BUILD_DIR="target"
+	FULL_INSTALLER="${BUILD_DIR}/handy-installer.sh"
+}
+
+clean() {
+	rm -rf ${BUILD_DIR}
 }
 
 package() {
+	mkdir ${BUILD_DIR}
 	cat ${INSTALLER} > ${FULL_INSTALLER}
 	echo "\n\n${SCRIPTS_SEPARATOR}" >> ${FULL_INSTALLER}
 	cat ${PAGE_CREATION_SCRIPT} >> ${FULL_INSTALLER}
@@ -16,4 +22,5 @@ package() {
 }
 
 setupVariables
+clean
 package
